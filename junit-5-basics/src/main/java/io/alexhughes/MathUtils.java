5c34f03486dce13cdbea6d79ed38bb83d9af12ca
Alex Hughes <ahughes.orlando@gmail.com>
2020-12-03 01:50:10 -0800

    Finished Practice

--- junit-5-basics/src/main/java/io/alexhughes/MathUtils.java
+++ junit-5-basics/src/main/java/io/alexhughes/MathUtils.java
@@ -5,4 +5,20 @@ public class MathUtils {
 	public int add(int a, int b) {
 		return a + b;
 	}
+	
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	
+	public int mult(int a, int b) {
+		return a * b;
+	}
+	
+	public int div(int a, int b) {
+		return a / b;
+	}
+	
+	public double computeArea(int radius) {
+		return radius * radius * Math.PI;
+	}
 }
--- junit-5-basics/src/test/java/io/alexhughes/MathUtilsTest.java
+++ junit-5-basics/src/test/java/io/alexhughes/MathUtilsTest.java
@@ -1,14 +1,59 @@
 package io.alexhughes;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+
+@DisplayName("From MathUtils class")
 class MathUtilsTest {
+	private MathUtils mathUtils;
 
-	@Test
-	void test() {
-		System.out.println("This test ran");
+	@BeforeAll
+	static void beforeAll() {
+		System.out.print("This is outputted before instances initalized.");
 	}
 
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
+	
+	@Test
+	@DisplayName("Divides 2 numbers")
+	void divide() {
+		assumeTrue(false);
+		assertEquals(25, mathUtils.div(5, 5), () -> "the div method should divide two numbers");
+	}
+	
+	@Test
+	@DisplayName("Divides a number by 0")
+	void divideByZero() {
+		assertThrows(ArithmeticException.class, () -> mathUtils.div(6, 0), () -> "the div method divides two numbers");
+	}
+
+	@Nested
+	class AddTest {
+		@Test
+		@DisplayName("Testing a method for +")
+		void addPositiveTest() {
+			assertEquals(2, mathUtils.add(1, 1), () -> "the add method should add two numbers.");
+		}
+
+		@Test
+		@DisplayName("Test a method for -")
+		void addNegativeTest() {
+			assertEquals(-2, mathUtils.add(-1, -1), () -> "the add method should add two numbers.");
+		}
+	}
+
+	@Tag("test")
+	@RepeatedTest(3)
+	@DisplayName("Testing a method to multiply two numbers")
+	void multiply() {
+		assertAll(() -> assertEquals(6, mathUtils.mult(2, 3), () -> "the mult method should multiply two numbers."),
+				() -> assertEquals(25, mathUtils.mult(5, 5), () -> "the mult method should multiply two numbers."),
+				() -> assertEquals(81, mathUtils.mult(9, 9), () -> "the mult method should multiply two numbers."));
+	}
 }
